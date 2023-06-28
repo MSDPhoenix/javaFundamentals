ff0faff2eaea1268cbd5147fb0675ed2df726562
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2023-06-27 17:51:37 -0700

    working on Four Pillars of OOP

---
+++ OOP/FourPillars/CappuccinoMaker.java
@@ -0,0 +1,13 @@
+
+public class CappuccinoMaker extends CoffeeMaker {
+
+    public CappuccinoMaker(int maxVolumeOz) {
+        super(maxVolumeOz);
+        this.milk = "whole";
+    }
+
+    public void makeCappuccino(String beans){
+        super.brew(beans);
+        System.out.println("Smooth.");
+    }
+}
